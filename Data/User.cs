b3a030264f88e84c68daa3c85a98e0b445d0918b
Mieszko <mirgosmieszko@gmail.com>
2022-11-14 20:56:32 +0100

    recreated tables

---
+++ Data/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Bank.NET___backend.Data
+{
+    public class User
+    {        
+        public int ID { get; set; }
+        public string PasswordHash { get; set; }//not sure if necessearry
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string GovermentId { get; set; }
+        public string Email { get; set; }
+        public string JobType { get; set; }
+        public decimal IncomeLevel { get; set; }
+    }
+}
