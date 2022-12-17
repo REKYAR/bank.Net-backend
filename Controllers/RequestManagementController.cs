a232d48b231f19c3a4445c2cafc04c6372ad748f
Mieszko <mirgosmieszko@gmail.com>
2022-12-17 11:17:36 +0100

    redierct changes

--- Controllers/RequestManagementController.cs
+++ Controllers/RequestManagementController.cs
@@ -84,7 +84,7 @@ namespace Bank.NET___backend.Controllers
             }
             //_sqlContext.Requests.Add(req);
             //send mail with request id
-            return Redirect($"api/requestManagement/offers/{req.RequestID}");
+            return Redirect($"https://bank-project-backend-dev.azurewebsites.net/api/requestManagement/offers/{req.RequestID}");
             //return Ok();
         }
 
