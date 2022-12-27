ebaac6adf615caab7c5881506ac04bf7b0b950a6
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2022-12-27 15:54:59 +0100

    New user API connector

---
+++ Models/APIConnectorResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace Bank.NET___backend.Models
+{
+    public enum APIConnectorActions
+    {
+        Continue,
+        ShowBlockPage,
+
+    }
+    
+    public abstract class BaseAPIConnectorResponse
+    {
+        public string version { get; set; }
+        public string action { get; set; }
+
+        public BaseAPIConnectorResponse(APIConnectorActions act)
+        {
+            version = "1.0.0";
+            action = act.ToString();
+        }
+    }
+
+
+    public class APIConnectorContinue: BaseAPIConnectorResponse
+    {
+        public APIConnectorContinue(): base(APIConnectorActions.Continue) { }
+    }
+
+
+    public class APIConnectorBlock: BaseAPIConnectorResponse
+    {
+        public string userMessage { get; set; }
+        
+        public APIConnectorBlock(string userMessage): base(APIConnectorActions.ShowBlockPage)
+        {
+            this.userMessage = userMessage;
+        }
+    }
+}
