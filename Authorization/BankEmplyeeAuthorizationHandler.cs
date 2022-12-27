883afd73a634f12dc0b8d59471cb1d9081873609
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2022-12-27 21:33:44 +0100

    Fix bank employee policy

--- Authorization/BankEmplyeeAuthorizationHandler.cs
+++ Authorization/BankEmplyeeAuthorizationHandler.cs
@@ -1,10 +1,17 @@
-﻿using Bank.NET___backend.Data;
+﻿using Azure.Core;
+using Bank.NET___backend.Data;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Bank.NET___backend.Authorization
 {
     public class BankEmplyeeAuthorizationHandler : AuthorizationHandler<GroupAuthorizationRequirement>
     {
+        private readonly SqlContext _sqlContext;
+
+        public BankEmplyeeAuthorizationHandler(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, GroupAuthorizationRequirement requirement)
         {
@@ -12,8 +19,11 @@ namespace Bank.NET___backend.Authorization
             var EmailClaim = Helpers.GetClaim(claims, "emails");
             if (EmailClaim != null)
             {
-
-                context.Succeed(requirement);
+                var res = _sqlContext.Admins.Where( a => a.Email == EmailClaim.Value).First();
+                if (res != null)
+                {
+                    context.Succeed(requirement);
+                }
             }
 
             return Task.CompletedTask;
