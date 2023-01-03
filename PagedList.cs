653cc7b6c272185076f0113fee1dfa76b5a4f7c3
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-01-03 20:35:31 +0100

    Fix paging

--- PagedList.cs
+++ PagedList.cs
@@ -22,7 +22,7 @@ namespace Bank.NET___backend
             AddRange(items);
         }
 
-        public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        public static PagedList<T> ToPagedList(IOrderedQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = source.Count();
             var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
